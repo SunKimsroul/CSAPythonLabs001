4251c2fc32597884ad644817fe79c2cc38f80060
SunKimsroul <157775563+SunKimsroul@users.noreply.github.com>
2024-03-11 08:17:08 +0700

    bookClass.py

--- librarymanagementsystem/bookClass.py
+++ librarymanagementsystem/bookClass.py
@@ -1,11 +1,10 @@
 class Book:
-    def __init__(self, title, author, year, editor):
+    def __init__(self, title, author, year=None, editor=None):
         self.title = title
         self.author = author
         self.year = year
         self.editor = editor
         self.is_checked_out = False
-
 
     def check_out(self):
         if not self.is_checked_out:
@@ -14,4 +13,9 @@ class Book:
         else:
             return f"{self.title} is already checked out."
 
-
+    def return_book(self):
+        if self.is_checked_out:
+            self.is_checked_out = False
+            return f"{self.title} has been returned."
+        else:
+            return f"{self.title} was not checked out."
